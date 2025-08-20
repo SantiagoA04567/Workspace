00bce2259695ebd9475f69706734fff1b2f64ef3
SantiagoA04567 <santago04524@gmail.com>
2025-08-20 16:27:25 -0500

    Add files via upload

---
+++ ejercicio javascript 1/EJERCICIO 1/main.js
@@ -0,0 +1,3 @@
+const titleElement = document.querySelector(".texto");
+
+titleElement.innerHTML = "Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente."
